6604a1cc28d8c8a592ad4e0d76948e119ec8e2df
BitterBallin <lennertbun@gmail.com>
2025-05-08 12:40:05 +0200

    Did one sided test, added target length to file name.

--- Arduino Orthosis/src/Python_Plotter_Saver_V3.py
+++ Arduino Orthosis/src/Python_Plotter_Saver_V3.py
@@ -139,14 +139,20 @@ for i in range(1, len(time_vals)):
     rpm_vals.append(rpm)
     rpm_time.append(t)
 
+
+
 # === Save to CSV ===
-test_number = 4
+#=============================================
+#=============================================
+
+test_number = 5
 voltage = 24
 wire_length = 155 # in cm
+position_target = int(target_vals[len(target_vals) // 2])
 current_date = datetime.datetime.now().date()
 # csv_filename = f"TB1_test_{test_number}_{current_date}.csv"
 # csv_filename = f"TB1_CHIRPTEST_TEST{test_number}_{voltage}V_{wire_length}CM_{current_date}.csv"
-csv_filename = f"TB2_RAMPTEST_ONESIDE_TEST_{test_number}_{voltage}V_{wire_length}CM_{current_date}.csv"
+csv_filename = f"TB2_RAMPTEST_ONESIDE_TARGET_{position_target}_TEST_{test_number}_{voltage}V_{wire_length}CM_{current_date}.csv"
 
 
 with open(csv_filename, mode='w', newline='') as file:
