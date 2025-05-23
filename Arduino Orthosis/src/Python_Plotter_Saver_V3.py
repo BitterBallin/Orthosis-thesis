e4bb6af0561dc4c18c6e2f126b63c459fb43fad5
BitterBallin <lennertbun@gmail.com>
2025-05-23 15:03:47 +0200

    Retested ROM of new orthosis with new markers. All videos made now going to do force testing of new orthosis.

--- Arduino Orthosis/src/Python_Plotter_Saver_V3.py
+++ Arduino Orthosis/src/Python_Plotter_Saver_V3.py
@@ -152,7 +152,7 @@ position_target = int(target_vals[len(target_vals) // 2]*1000)
 current_date = datetime.datetime.now().date()
 # csv_filename = f"TB1_test_{test_number}_{current_date}.csv"
 # csv_filename = f"TB1_CHIRPTEST_TEST{test_number}_{voltage}V_{wire_length}CM_{current_date}.csv"
-csv_filename = f"TB2_NEWDESIGN_RAMPTEST_ONESIDED_TARGET_{position_target}mm_TEST_{test_number}_{voltage}V_{wire_length}CM_{current_date}.csv"
+csv_filename = f"TB2_NEWDESIGN_RAMPTEST_V2BLUE_ONESIDED_TARGET_{position_target}mm_TEST_{test_number}_{voltage}V_{wire_length}CM_{current_date}.csv"
 # csv_filename = f"TB2_SENSORDEBUGGING_RAMPTEST_ONESIDE_TARGET_{position_target}_TEST_{test_number}_{voltage}V_{wire_length}CM_{current_date}.csv"
 
 
